6ce83ef9847589a3ed1039c4062b62d1e554c4e4
Amrita3292 <167297651+Amrita3292@users.noreply.github.com>
2024-06-06 11:43:52 +0530

    About

--- Project_03/src/App.jsx
+++ Project_03/src/App.jsx
@@ -15,16 +15,37 @@ function App() {
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "red"}}
           >Red</button>
+          
           <button
           onClick={() => setColor("green")}
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "green"}}
           >Green</button>
+          
           <button
           onClick={() => setColor("blue")}
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "blue"}}
           >Blue</button>
+          
+          <button
+          onClick={() => setColor("aqua")}
+          className="outline-none px-4 py-1 rounded-full text-dark shadow-lg"
+          style={{backgroundColor: "aqua"}}
+          >Aqua</button>
+          
+          <button
+          onClick={() => setColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full text-dark shadow-lg"
+          style={{backgroundColor: "yellow"}}
+          >Yellow</button>
+          
+          <button
+          onClick={() => setColor("pink")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "pink"}}
+          >Pink</button>
+
         </div>
       </div>
     </div>
